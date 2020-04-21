7b167e5a643d78d030440b3da5967b8da21b9afb
Arseburger <japaneesegena@gmail.com>
2020-04-22 01:07:07 +0300

    Commit1

--- AR_Painting/ViewController.swift
+++ AR_Painting/ViewController.swift
@@ -9,67 +9,74 @@
 import UIKit
 import SceneKit
 import ARKit
+import Photos
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+  
+  @IBOutlet var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
+    sceneView.layer.cornerRadius = 30.0
+    // Set the view's delegate
+    sceneView.delegate = self
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = false
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
+    // Create a new scene
+    let scene = SCNScene(named: "art.scnassets/ship.scn")!
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+    // Set the scene to the view
+    sceneView.scene = scene
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
+    // Create a session configuration
+    let configuration = ARWorldTrackingConfiguration()
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
+    // Run the view's session
+    sceneView.session.run(configuration)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+    // Pause the view's session
+    sceneView.session.pause()
+  }
+  
+  // MARK: - ARSCNViewDelegate
+  
+  /*
+   // Override to create and configure nodes for anchors added to the view's session.
+   func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+   let node = SCNNode()
+   
+   return node
+   }
+   */
+  
+  func session(_ session: ARSession, didFailWithError error: Error) {
+    // Present an error message to the user
+    
+  }
+  
+  func sessionWasInterrupted(_ session: ARSession) {
+    // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    
+  }
+  
+  func sessionInterruptionEnded(_ session: ARSession) {
+    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    
+  }
+  
+  //  func screenShot() {
+  //    print(sceneView.session.currentFrame?.anchors)
+  //  }
+  
 }
